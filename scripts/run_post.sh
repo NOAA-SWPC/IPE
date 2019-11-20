247575e577cb223d9e717fd0f912bfd51e6ab69a
Adam Kubaryk <Adam.Kubaryk@noaa.gov>
2019-11-20 15:31:01 -0500

    machine agnosticism and fixing hard-coded output path.

--- scripts/run_post.sh
+++ scripts/run_post.sh
@@ -4,18 +4,20 @@
 # IPE_State.apex.*.h5 location
 IPE_INPUT_DIRECTORY=/scratch1/NCEPDEV/stmp2/Tzu-Wei.Fang/GSMWAM-IPE_dynamo
 # where you want the IPE_Params.geo.*.nc4 files
-#IPE_OUTPUT_DIRECTORY=$IPE_INPUT_DIRECTORY/netcdf
-IPE_OUTPUT_DIRECTORY=/scratch1/NCEPDEV/stmp4/Adam.Kubaryk/tec_geo_test
+IPE_OUTPUT_DIRECTORY=$IPE_INPUT_DIRECTORY/netcdf
 ###### USUALLY NO NEED TO CHANGE ANYTHING BELOW THIS LINE ######
-
-## HERA GRID LOCATION
-IPE_GRID=/scratch1/NCEPDEV/swpc/WAM-IPE_DATA/IPE_FIX/IPE_Grid.h5
-## MODULEFILE LOAD
-module purge
-module use -a ../../NEMS/src/conf
-module use -a /contrib/modulefiles
-module load modules.nems
-module load anaconda
+if [[ -e /scratch1 ]] ; then
+  ## HERA GRID LOCATION
+  IPE_GRID=/scratch1/NCEPDEV/swpc/WAM-IPE_DATA/IPE_FIX/IPE_Grid.h5
+  ## MODULEFILE LOAD
+  module use -a /contrib/modulefiles
+  module load anaconda
+elif [[ -e /gpfs/dell2 ]] ; then
+  ## WCOSS DELL P3 GRID LOCATION
+  IPE_GRID=/gpfs/dell2/emc/modeling/noscrub/Adam.Kubaryk/refactored_ipe_input_decks/IPE_Grid.h5
+  ## MODULEFILE LOAD
+  module load python/2.7.14
+fi
 ## DIRECTORY SETUP
 mkdir -p $IPE_OUTPUT_DIRECTORY
 ## RUN
